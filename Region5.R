d93a2bfeb4791839a006b33d936abfcd0e06d90f
Jess Kunke <jessicakunke@gmail.com>
2025-05-07 20:29:35 -0700

    Region 5 results

--- Region5.R
+++ Region5.R
@@ -37,6 +37,7 @@ pwdist_input_dir = "pwdists/input/"
 pwdist_predsobs_dir = "pwdists/output_predsobs/"
 # where to find the obs-obs pairwise distance results from the cluster
 pwdist_obsobs_dir = "pwdists/output_obsobs/"
+est_pred_results_dir = "Region5_results/"
 
 # preprocess streams data before identifying benchmark subnetworks --------
 
@@ -197,7 +198,8 @@ preds = left_join(
   streams %>%
     st_drop_geometry() %>%
     data.frame() %>%
-    select(ptID, Development, Agriculture, Elevation, Water_Area, 
+    # need LENGTHKM later for scaling up from point predictions to regional estimate
+    select(ptID, LENGTHKM, Development, Agriculture, Elevation, Water_Area, 
            Ann_Runoff, Baseflow, Ann_Temp, Hydro_Alter, Fldplain_Dis),
   by="ptID"
 )
@@ -227,104 +229,303 @@ fish_region5 = fish %>%
 save(preds, obs, fish_region5, 
      file = paste0(pwdist_input_dir, "preds_obs_fish_for_estimation.RData"))
 
-
-# # this is the old code I used for the first Region 5 results
-# count_each_species_in_fish(
-#   fish, out_dir = fish_path, 
-#   filename = "nobs_by_species_Region05.csv"
-# )
-# # 309 species
-# # nobs_by_species %>% filter(nCOMIDs > 15) # 202 species left
-# 
-# # how about we make predictions for all species with (non-zero) obs in at least 
-# # as many COMIDs as the number of neighbors we're using?
-# # in a sense, they all have obs at all the COMIDs, since the surveyers count 
-# # all the fish they find and fish that are not recorded were not observed at 
-# # that time and place, but I mean non-zero observations
-# # species_for_prediction = filter(nobs_by_species, nCOMIDs > 15)$Common_Name
-# 
-# get_obs_layer_for_each_species(
-#   fish, 
-#   out_dir = paste0(fish_path, "individual_species/"), 
-#   region = "Region05"
-# )
+count_each_species_in_fish(
+  fish_region5,
+  out_dir = est_pred_results_dir, 
+  filename = "nobs_by_species", 
+  write_to_csv = TRUE,
+  write_to_rda = TRUE,
+  return_obj = FALSE)
 
 
 
 
 # Estimation and prediction for each species ------------------------------------
 
+# for previous version of code, see Region5_est_pred_pipeline.R located in
+# ~/Documents/UW/Research/Julian-Olden-SAFS/BRISC_stream/extras/
+
 pwdist_obsobs_dir = "Region5_cluster_results/"
 pwdist_predsobs_dir = "Region5_cluster_results/"
 
-# needed for estimation
-# loads obsobs_dist, obsobs_wt
-load(paste0(pwdist_obsobs_dir, "obsobs_dist_wt.rda"))
-# loads obs_neighbors
-load(paste0(pwdist_obsobs_dir, "obs_neighbors.rda"))
-# loads preds, obs, fish_region5
+# needed for estimation and prediction: preds_obs_fish_for_estimation.RData loads preds, obs, fish_region5
+# - if you also want streams, load this first then overwrite preds and obs with the next line
 load(paste0(pwdist_input_dir, "preds_obs_pwdist_input_data_Region5.RData"))
-load(paste0(pwdist_input_dir, "preds_obs_fish_for_estimation.RData"))
+load(paste0(pwdist_input_dir, "preds_obs_fish_for_estimation_Region5.RData"))
 
-common_name = "Creek Chub"
-this_species = fish_region5 %>%
-  filter(Common_Name == common_name) %>%
-  select(COMID, DensityPer100m)
+# needed for estimation: loads obs_neighbors
+load(paste0(pwdist_obsobs_dir, "obs_neighbors.rda"))
 
-if(nrow(this_species) != n_distinct(this_species$COMID)){
-  stop("COMID is not a unique identifier for this_species")
+# needed for prediction: loads obsobs_dist, obsobs_wt
+load(paste0(pwdist_obsobs_dir, "obsobs_dist_wt.rda"))
+# moved the next two lines from BRISC_prediction_stream to here
+obsobs_dist = obsobs_dist + t(obsobs_dist)
+obsobs_wt = obsobs_wt + t(obsobs_wt) - diag(nrow(obs)) # this is the slow step (4 sec)
+# needed for prediction: loads pred_neighbors
+load(paste0(pwdist_predsobs_dir, "pred_neighbors.rda"))
+
+## for all species ----------------------------
+
+load(paste0(est_pred_results_dir, "nobs_by_species.rda"))
+
+results_by_species = list()
+nspecies = length(nobs_by_species$Common_Name)
+
+mem.maxVSize(20000)
+tic("Estimation and prediction for all species")
+for(s_ind in 1:nspecies){ #for(s_ind in 1:nspecies){ # for(s_ind in 1:5){
+  common_name = nobs_by_species$Common_Name[s_ind]
+  cat("\n----------------------------------------------", fill = TRUE)
+  cat(paste0("Species ", s_ind, " of ", nspecies, ": ", common_name), fill = TRUE)
+  cat("----------------------------------------------", fill = TRUE)
+  
+  results = tryCatch({
+    get_regional_estimate_for_species(
+      common_name, 
+      fish_region5, obs, preds,
+      obs_neighbors, pred_neighbors,
+      obsobs_dist, obsobs_wt,
+      nugget_status = 1, verbose = FALSE)
+  }, error = function(err) {
+    # error handler picks up where error was generated
+    print(err)
+    return(NA)
+  })
+  
+  results_by_species[[common_name]] = results
+  
+}
+toc()
+save(results_by_species,
+     file = paste0(est_pred_results_dir, "Region5_est_pred_results.RData"))
+beep(3)
+
+
+## set all weights to 1 ----------------------------
+
+load(paste0(est_pred_results_dir, "nobs_by_species.rda"))
+
+results_by_species = list()
+nspecies = length(nobs_by_species$Common_Name)
+
+# set weights to 1:
+obsobs_wt = matrix(1, nrow = n, ncol = n)
+obs_neighbors$nnWt = rep(1, length(obs_neighbors$nnWt))
+obs_neighbors$Dwt = rep(1, length(obs_neighbors$Dwt))
+pred_neighbors$nnWght = rep(1, length(pred_neighbors$nnWght))
+
+mem.maxVSize(20000)
+tic("Estimation and prediction for all species")
+for(s_ind in 1:nspecies){ #for(s_ind in 1:nspecies){ # for(s_ind in 1:5){
+  common_name = nobs_by_species$Common_Name[s_ind]
+  cat("\n----------------------------------------------", fill = TRUE)
+  cat(paste0("Species ", s_ind, " of ", nspecies, ": ", common_name), fill = TRUE)
+  cat("----------------------------------------------", fill = TRUE)
+  
+  results = tryCatch({
+    get_regional_estimate_for_species(
+      common_name, 
+      fish_region5, obs, preds,
+      obs_neighbors, pred_neighbors,
+      obsobs_dist, obsobs_wt,
+      nugget_status = 1, verbose = FALSE)
+  }, error = function(err) {
+    # error handler picks up where error was generated
+    print(err)
+    return(NA)
+  })
+  
+  results_by_species[[common_name]] = results
+  
+}
+toc()
+save(results_by_species,
+     file = paste0(est_pred_results_dir, "Region5_est_pred_results.RData"))
+beep(3)
+
+
+# Diagnostics ----------------------
+
+plot_pred_density_map = function(streams, preds_supp, common_name, out_dir){
+  usa <- st_as_sf(maps::map("state", fill=TRUE, plot=FALSE)) %>%
+    st_transform(crs = st_crs(streams))
+  
+  streams = streams %>%
+    left_join(select(preds_supp, COMID, DensityPer100m_pred), by="COMID") %>%
+    mutate(DensityPer100m_pred = ifelse(DensityPer100m_pred < 0, 0.00001, DensityPer100m_pred))
+  
+  print(summary(streams$DensityPer100m_pred))
+  
+  # plot of Subnetwork 6 with Subnetwork 5 highlighted in colors
+  ggplot() +
+    geom_sf(data = usa,
+            color = "#2b2b2b",
+            fill = "white") +
+    geom_sf(data = st_simplify(streams,
+                               preserveTopology = FALSE,
+                               dTolerance = 1000),
+            aes(color = DensityPer100m_pred), linewidth = 0.3) +
+    # scale_color_gradient(trans = 'log') +
+    scale_color_continuous(breaks=quantile(streams$DensityPer100m_pred)) +
+    coord_sf(xlim = c(-90, -75),
+             ylim = c(35, 45),
+             default_crs = sf::st_crs(4326)) +
+    labs(title = common_name) +
+    ggthemes::theme_map() +
+    theme(legend.position = "right")
+  
+  ggsave(filename = paste0(out_dir, "Region5_pred_density_map_", str_replace_all(common_name, " ", "_"), ".png"),
+         width = 4, height = 4, units = "in")
 }
 
-obs_this_species = obs %>%
-  left_join(this_species, by = "COMID") %>%
-  # set to 0 if NA because this species was not observed there
-  mutate(DensityPer100m = replace_na(DensityPer100m, 0))
 
-tic("Estimation"); start_time = Sys.time()
-estimation = BRISC_estimation_stream(
-  coords = as.matrix(1:nrow(obs_this_species)),
-  y = as.matrix(obs_this_species$DensityPer100m),
-  x = get_X(obs_this_species),
-  neighbor = obs_neighbors,
-  cov.model = "exponential",
-  nugget_status = 1,
-  verbose = TRUE
+
+# - update total counts to look at density less than or equal to -1 and 
+#   median neg/median pos density
+#   - this includes correlation of densities across all sites for each species (309 numbers)
+# - correlation between pred and obs density for all species at each site 
+#   (result: 8907 values, each of which is a correlation of 309 pairs of values)
+# - plot of predicted vs observed counts... for each species? 309 plots?
+
+q = nrow(preds)
+n = n_distinct(fish_region5$COMID)
+n_species = nrow(nobs_by_species)
+
+# col 1 = COMID, cols 2 through end are the 309 different species observed in Region 5
+density_by_site_obs = matrix(nrow = n, ncol = n_species+1)
+density_by_site_obs[,1] = preds$COMID[1:n]
+
+density_by_site_pred = matrix(nrow = n, ncol = n_species+1)
+density_by_site_pred[,1] = preds$COMID[1:n]
+
+total_counts = data.frame(
+  # species name
+  species = nobs_by_species$Common_Name,
+  # number of COMIDs on which this species was observed
+  nCOMIDs = nobs_by_species$nCOMIDs,
+  # proportion of COMIDs on which any fish was observed on which
+  # *THIS* species was observed
+  p_COMIDs_with_obs = nobs_by_species$nCOMIDs/n,
+  # total observed count
+  obs_total_count = nobs_by_species$TotalCount,
+  # total predicted count on COMIDs where *any fish* were observed
+  pred_total_count_wherefishobs = NA,
+  # total predicted count on COMIDs where *this species* was observed
+  pred_total_count_wherethisspecobs = NA,
+  # total predicted Region 5 population size
+  # (sum of 10*DensityPer100m*LENGTHKM over COMIDs with nonnegative predicted densities)
+  regional_estimate = NA,
+  # proportion of predicted densities that are <= -1
+  # total_counts$p_neg[s] = sum(DensityPer100m <= -1)/q
+  p_neg = NA,
+  # proportion of predicted densities *on COMIDs with observations of any 
+  # fish species* that are <= -1
+  # sum(DensityPer100m[1:n] <= -1)/n
+  p_neg_obs = NA,
+  # proportion of predicted densities *on COMIDs with observations of any
+  # fish species* for which (1) predicted density <= -1 *AND* (2) this species 
+  # was not observed
+  # sum(DensityPer100m[1:n][obs_density==0] < -1)/n
+  p_neg_obs0 = NA,
+  # median of the negative predicted densities
+  # median(DensityPer100m[1:n][DensityPer100m[1:n] < 0])
+  median_neg = NA,
+  # median of the positive predicted densities
+  # median(DensityPer100m[1:n][DensityPer100m[1:n] > 0])
+  median_pos = NA,
+  # correlation between predicted and observed density on COMIDs in which
+  # any fish species was observed
+  # cor(DensityPer100m[1:n], obs_density)
+  cor_obs_pred = NA,
+  # correlation between predicted and observed density on COMIDs in which
+  # *THIS* fish species was observed
+  # cor(DensityPer100m[1:n][obs_density > 0], obs_density[obs_density > 0])
+  cor_obs_pred_nz = NA,
+  # estimation runtime
+  estimation_time = NA,
+  estimation_units = NA,
+  # prediction runtime
+  prediction_time = NA,
+  prediction_units = NA
 )
-toc(); stop_time = Sys.time()
-# runtimes[5] = as.numeric(difftime(stop_time, start_time), units="secs")
 
-# params = c(
-#   estimation$Beta, # beta (intercept, elevation)
-#   estimation$Theta # sigma.sq, tau.sq, phi
-# )
-# params[5] = 1/params[5] # convert phi to lambda (range parameter)
-# names(params)[5] = "lambda"
-
-# save estimated parameter values, n_neighbors, species, and runtime
-
-# prediction
-# load pred_neighbors object
-load(paste0(pwdist_predsobs_dir, "pred_neighbors.rda"))
-prediction = BRISC_prediction_stream(
-  BRISC_Out = estimation,
-  coords.0 = as.matrix(1:nrow(preds)),
-  X.0 = get_X(preds),
-  # X.0 = streams %>%
-  #       st_drop_geometry() %>%
-  #       select(Elevation) %>%
-  #       # select(Elevation, Water_Area,
-  #       #        Ann_Runoff, Baseflow, Ann_Temp,
-  #       #        Development, Agriculture,
-  #       #        Hydro_Alter, Fldplain_Dis) %>%
-  #       mutate(Intercept = 1, .before = Elevation) %>%
-  #       as.matrix(),
-  neighbor = pred_neighbors,
-  obsobs_dist = obsobs_dist,
-  obsobs_wt = obsobs_wt,
-  verbose = TRUE
+params = data.frame(
+  beta_1 = NA,
+  beta_2 = NA,
+  beta_3 = NA,
+  beta_4 = NA,
+  beta_5 = NA,
+  beta_6 = NA,
+  beta_7 = NA,
+  beta_8 = NA,
+  beta_9 = NA,
+  beta_10 = NA,
+  theta_1 = NA,
+  theta_2 = NA,
+  theta_3 = NA
 )
-toc(); stop_time = Sys.time()
-# runtimes[7] = as.numeric(difftime(stop_time, start_time), units="secs")
 
+tic("diagnostics loop"); for(s in 1:nrow(total_counts)){ #for(s in 1:10){
+  cat(paste0(s, " "))
+  species = total_counts$species[s]
+  
+  if(!is.na(results_by_species[[s]][1])){
+    
+    DensityPer100m = results_by_species[[s]]$preds_supp$DensityPer100m_pred
+    CountReachTotal = results_by_species[[s]]$preds_supp$CountReachTotal_pred
+    obs_density = results_by_species[[s]]$preds_supp$obs_density
+    
+    # predicted total count
+    total_counts$pred_total_count_wherefishobs[s] = sum(CountReachTotal[1:n][CountReachTotal[1:n] >=0])
+    total_counts$pred_total_count_wherethisspecobs[s] = sum(CountReachTotal[1:n][CountReachTotal[1:n] >=0 & obs_density[1:n] > 0])
+    total_counts$regional_estimate[s] = sum(CountReachTotal[CountReachTotal >=0])
+    total_counts$p_neg[s] = sum(DensityPer100m <= -1)/q
+    total_counts$p_neg_obs[s] = sum(DensityPer100m[1:n] <= -1)/n
+    total_counts$p_neg_obs0[s] = sum(DensityPer100m[1:n][obs_density[1:n]==0] < -1)/n
+    total_counts$median_neg[s] = median(DensityPer100m[1:n][DensityPer100m[1:n] < 0])
+    total_counts$median_pos[s] = median(DensityPer100m[1:n][DensityPer100m[1:n] > 0])
+    total_counts$cor_obs_pred[s] = cor(DensityPer100m[1:n], obs_density[1:n])
+    total_counts$cor_obs_pred_nz[s] = cor(DensityPer100m[1:n][obs_density[1:n] > 0], 
+                                          obs_density[1:n][obs_density[1:n] > 0])
+    # ensure all estimation and predictions times have the same units
+    units(results_by_species[[s]]$estimation_time) = "secs"
+    units(results_by_species[[s]]$prediction_time) = "secs"
+    total_counts$estimation_time[s] = results_by_species[[s]]$estimation_time
+    total_counts$prediction_time[s] = results_by_species[[s]]$prediction_time
+    total_counts$prediction_units[s] = "secs"
+    total_counts$estimation_units[s] = "secs"
+    params[s,] = c(results_by_species[[s]]$estimation$Beta, results_by_species[[s]]$estimation$Theta)
+    
+    density_by_site_pred[, s+1] = DensityPer100m[1:n]
+    density_by_site_obs[, s+1] = obs_density[1:n]
+  }
+}; cat("\n"); toc()
+# diagnostics loop: 55.643 sec elapsed 
+
+diagnostics = cbind(total_counts, params) %>%
+  rename(
+    Intercept = beta_1,
+    Elevation = beta_2,
+    Water_Area = beta_3,
+    Ann_Runoff = beta_4,
+    Baseflow = beta_5,
+    Ann_Temp = beta_6,
+    Development = beta_7,
+    Agriculture = beta_8,
+    Hydro_Alter = beta_9,
+    Fldplain_Dis = beta_10,
+    SigmaSq = theta_1,
+    TauSq = theta_2,
+    Phi = theta_3
+  )
+
+cor_density_by_site = rep(NA, n)
+for(i in 1:n){
+  cor_density_by_site[i] = cor(density_by_site_obs[i,2:(n_species+1)], 
+                               density_by_site_pred[i,2:(n_species+1)])
+}
+
+save(diagnostics, cor_density_by_site,
+     file = paste0(est_pred_results_dir, "Region5_diagnostics.RData"))
 
 
